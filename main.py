ec2ce834bd9a96aadf384f35888fcbd148fad7d8
Jelle van der Zwaag <contact@tulipa.in>
2021-06-12 12:42:12 +0200

    First commit.

---
+++ main.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+class Board():
+
+    def __init__(self, players, empty_square=' '):
+        self.empty_square = empty_square
+        self.players = players
+        self.current_player = players[0]
+        self.squares = [empty_square]*9
+
+    
+    def valid_move(self, square):
+        try:
+            square = int(square)
+        except ValueError as e:
+            print('Give a number.')
+            return False
+
+        if square >= len(self.squares):
+            print('Out of bounds.')
+            return False
+
+        if self.squares[square] in self.players:
+            print('Already occupied.')
+            return False
+        
+        return True
+
+    
+    def next_player(self):
+        
+        ix = self.players.index(self.current_player)
+        # to get a cyclical index selection take the mod of the number of players
+        next_ix = ( ix + 1 ) % len(self.players)
+        self.current_player = self.players[next_ix]
+
+        return self.current_player
+
+
+    def move(self, square):
+
+        # self.squares.insert(int(square), self.current_player)
+        self.squares[int(square)] = self.current_player
+
+        return True
+
+    def has_won(self):
+
+        return (self.squares[0:3] != [self.empty_square]*3 and self.squares[0] == self.squares[1] == self.squares[2]) or \
+               (self.squares[3:6] != [self.empty_square]*3 and self.squares[3] == self.squares[4] == self.squares[5]) or \
+            (self.squares[6:9] != [self.empty_square]*3 and self.squares[6] == self.squares[7] == self.squares[8]) or \
+                ([self.squares[0], self.squares[3], self.squares[6]] != [self.empty_square]*3 and self.squares[0] == self.squares[3] == self.squares[6]) or \
+                ([self.squares[1], self.squares[4], self.squares[7]] != [self.empty_square]*3 and self.squares[1] == self.squares[4] == self.squares[7]) or \
+                ([self.squares[2], self.squares[5], self.squares[8]] != [self.empty_square]*3 and self.squares[2] == self.squares[5] == self.squares[8]) or \
+                ([self.squares[2], self.squares[4], self.squares[6]] != [self.empty_square]*3 and self.squares[2] == self.squares[4] == self.squares[6]) or \
+                ([self.squares[0], self.squares[4], self.squares[8]] != [self.empty_square]*3 and self.squares[0] == self.squares[4] == self.squares[8])
+
+
+
+    def __str__(self):
+
+        line1 = f'{self.squares[0]} | {self.squares[1]} | {self.squares[2]}'
+        line2 = f'{self.squares[3]} | {self.squares[4]} | {self.squares[5]}'
+        line3 = f'{self.squares[6]} | {self.squares[7]} | {self.squares[8]}'
+        between = f'-'*9
+
+        return '\n'.join([line1, between, line2, between, line3])
+
+
+if __name__ == '__main__':
+    players = ['X', 'O']
+    board = Board(players)
+
+    print(board)
+    print()
+    while not board.has_won():
+
+        valid_move = False
+        while not valid_move:
+            square = input(f'Player {board.current_player}. What is your move [0-{len(board.squares)-1}]? ')
+            valid_move = board.valid_move(square)
+            if valid_move:
+                board.move(square)
+            if not board.has_won():
+                board.next_player()
+        print()
+        print(board)
+    
+    print(f'Player {board.current_player} has won!')
---
+++ tests.py
@@ -0,0 +1,94 @@
+import unittest
+from main import Board
+
+class TestBoard(unittest.TestCase):
+
+    def test_init_board(self):
+        board = Board(['X', 'O'], empty_square='a')
+
+        board.squares = ['a']*9
+        board.current_player = 'X'
+        board.players = ['X', 'O']
+        board.empty_square = 'a'
+
+    def test_move(self):
+
+        board = Board(['X', 'O'], empty_square='a')
+        board.squares = ['O', 'X', 'O', 'O', ' ', ' ', 'O', 'X', 'O' ]
+        board.move('5')
+        self.assertEqual(board.squares, ['O', 'X', 'O', 'O', ' ', 'X', 'O', 'X', 'O' ])
+
+
+
+    def test_winning_board(self):
+        board = Board(['X', 'O'])
+        board.squares = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.assertTrue(board.has_won())
+
+        board.squares = [' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ']
+        self.assertTrue(board.has_won())
+
+        board.squares = [' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X' ]
+        self.assertTrue(board.has_won())
+
+        board.squares = ['X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X' ]
+        self.assertFalse(board.has_won())
+
+        board.squares = [' ', 'X', 'O', ' ', 'O', ' ', ' ', 'X', 'X' ]
+        self.assertFalse(board.has_won())
+
+        board.squares = [' ', ' ', 'O', ' ', 'O', 'O', ' ', 'X', 'O' ]
+        self.assertTrue(board.has_won())
+
+        board.squares = ['O', 'X', 'O', ' ', 'O', ' ', ' ', 'X', 'O' ]
+        self.assertTrue(board.has_won())
+
+        board.squares = ['O', 'X', 'O', 'O', ' ', ' ', 'O', 'X', 'O' ]
+        self.assertTrue(board.has_won())
+
+    def test_start_player(self):
+        board = Board(['X', 'O'])
+
+        self.assertEqual(board.current_player, 'X')
+
+    
+    def test_next_player(self):
+
+        board = Board(['X', 'O'])
+
+        board.next_player()
+        self.assertEqual(board.current_player, 'O')
+        board.next_player()
+        self.assertEqual(board.current_player, 'X')
+        board.next_player()
+        self.assertEqual(board.current_player, 'O')
+        
+        board_three_players = Board(['X', 'O', 'Y'])
+
+        board_three_players.next_player()
+        self.assertEqual(board_three_players.current_player, 'O')
+        board_three_players.next_player()
+        self.assertEqual(board_three_players.current_player, 'Y')
+        board_three_players.next_player()
+        self.assertEqual(board_three_players.current_player, 'X')
+
+    def test_valid_move(self):
+
+        board = Board(['X', 'O'])
+        board.squares = [' ', 'X', 'O', ' ', 'O', ' ', ' ', 'X', 'X' ]
+
+        self.assertFalse(board.valid_move('a'))
+        self.assertFalse(board.valid_move('!'))
+        self.assertFalse(board.valid_move('1'))
+        self.assertFalse(board.valid_move('11'))
+        self.assertFalse(board.valid_move('2'))
+        self.assertFalse(board.valid_move('qwertzuterwrtzr'))
+        self.assertTrue(board.valid_move('0'))
+        self.assertTrue(board.valid_move('3'))
+        self.assertTrue(board.valid_move('5'))
+        self.assertTrue(board.valid_move('6'))
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
